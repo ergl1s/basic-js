f030395ae9cc7200b59dfc14b607825b37c3bfe2
ergl1s <bibikov2000@mail.ru>
2020-02-17 21:03:33 +0300

    feat: solution of the count-cats and carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,11 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(string) {
+  if (string == undefined || typeof string != "string") 
+    return false; 
+  let temp = parseFloat(string);
+  if (Number.isNaN(temp) || temp > 15 || temp <= 0)
+    return false; 
+  return Math.ceil(Math.log(MODERN_ACTIVITY / temp) / (0.693 / HALF_LIFE_PERIOD));
 };
