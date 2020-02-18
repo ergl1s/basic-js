46ff582c607ac9c22bc2fa110e7ed773c4745a41
ergl1s <bibikov2000@mail.ru>
2020-02-18 21:31:04 +0300

    feat: solution of the dream-team and what-season

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,4 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false; 
+  return members.filter(element => typeof element == "string").map(element => element.trim()[0].toUpperCase()).sort().join("");
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,10 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date instanceof Date) throw Error; 
+  let date2 = new Date(); 
+  if (date - date2 == 0 || date == undefined) return 'Unable to determine the time of year!';
+  temp = date.getMonth(); 
+  if (temp == 11 || temp == 0 || temp == 1) return "winter";
+  if (temp == 2 || temp == 3 || temp == 4) return "spring";
+  if (temp == 5 || temp == 6 || temp == 7) return "summer"; 
+  if (temp == 8 || temp == 9 || temp == 10) return "fall";
 };
