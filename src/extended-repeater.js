f004cfccc3bcbc59939adb08e78a04e68e3dfa8b
ergl1s <bibikov2000@mail.ru>
2020-02-22 19:24:31 +0300

    full solution of the extended-repeater and vigenere-cipher

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,21 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  str = String(str); 
+  let string = "";
+  if (options.repeatTimes === undefined) options.repeatTimes = 1; 
+  if (options.separator === undefined) options.separator = '+'; 
+  if (options.addition === undefined) options.addition = '';
+  if (options.additionRepeatTimes === undefined) options.additionRepeatTimes = 1; 
+  if (options.additionSeparator === undefined) options.additionSeparator = '|';
+  options.addition = String(options.addition);
+
+  for(let i = 0 ; i < options.repeatTimes; i++) {
+    string += str; 
+    for(let j = 0 ; j < options.additionRepeatTimes - 1; j++) {
+      string += options.addition;
+      string += options.additionSeparator;
+    }
+    string += options.addition; 
+    string += options.separator; 
+  }
+  return string.slice(0, -options.separator.length); 
 };
-  
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,59 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  constructor (bool) {
+      if (bool == true || bool === undefined) {
+        this.flag = true; 
+      }
+      else this.flag = false; 
+  };
+  encrypt(message, key) {
+    if(message === undefined || key === undefined)
+      throw Error;
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let array = []; 
+    let keyLength = key.length; 
+    let shift = 0; 
+    for(let i = 0; i < message.length; i++) {
+      let code = message.charCodeAt(i);
+      array.push(code);
+      if (code >= 65 && code <= 90) {
+        array[i] += key.charCodeAt((i-shift) % keyLength)-65;
+        if (array[i] > 90) array[i] -= 26; 
+      }
+      else shift++;
     }
+    if (this.flag) {
+      return String.fromCharCode(...array);
+    } 
+    else {
+      return String.fromCharCode(...array.reverse());
+    }
+  }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  decrypt(string, key) {
+    if(string === undefined || key === undefined)
+      throw Error;
+    string = string.toUpperCase();
+    key = key.toUpperCase();
+    let array = []; 
+    let keyLength = key.length; 
+    let shift = 0; 
+    for(let i = 0; i < string.length; i++) {
+      let code = string.charCodeAt(i);
+      array.push(code);
+      if (code >= 65 && code <= 90) {  
+        array[i] -= key.charCodeAt((i-shift) % keyLength)-65;
+        if (array[i] < 65) array[i] += 26; 
+      }
+      else shift++;
     }
+    if (this.flag) {
+      return String.fromCharCode(...array);
+    } 
+    else {
+      return String.fromCharCode(...array.reverse());
+    }
+  }
 }
 
 module.exports = VigenereCipheringMachine;
